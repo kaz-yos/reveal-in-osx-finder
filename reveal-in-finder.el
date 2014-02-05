91e32580229d42cedd2006dc47ec56a99d1e8c86
kaz-yos <kazky@mac.com>
2014-02-05 07:57:58 -0500

    fixed typo

--- reveal-in-finder.el
+++ reveal-in-finder.el
@@ -49,7 +49,7 @@ In a dired buffer, it will open the current directory."
       ;; If path is empty, there is no file name. Use the default-directory variable.
       ;; This should work in a dired buffer.
       (setq dir (expand-file-name default-directory)))	
-    (reveal-in-finder-as dir file) ; global variables are required to pass it to the helper.
+    (reveal-in-finder-as dir file) ; Global variables are required to pass them to the helper.
     ))
 
 ;; AppleScript helper function. Thanks milkeypostman for suggestions.
@@ -57,18 +57,18 @@ In a dired buffer, it will open the current directory."
 (defun reveal-in-finder-as (dir file)
   "A helper function for reveal-in-finder.
 This function runs the actual AppleScript."
-  (let* ((revealpath (if file		   ; define revealpath local variable.
+  (let* ((revealpath (if file		   ; Define revealpath local variable.
 			 (concat dir file) ; dir/file if file name available.
 		       dir))		   ; dir only if not.
-	 (script			   ; define script variable using revealpath and text.
+	 (script			   ; Define script variable using revealpath and text.
 	  (concat
 	   "set thePath to POSIX file \"" revealpath "\"\n"
 	   "tell application \"Finder\"\n"
 	   " set frontmost to true\n"
 	   " reveal thePath \n"
 	   "end tell\n")))		   ; let* definitions end here.
-    ;; (message script)			   ; check the text output.
-    (start-process "osascript-getinfo" nil "osascript" "-e" script) ; Run AppleScript
+    ;; (message script)			   ; Check the text output.
+    (start-process "osascript-getinfo" nil "osascript" "-e" script) ; Run AppleScript.
     ))
 
 (provide 'reveal-in-finder)
