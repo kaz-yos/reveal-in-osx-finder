2c0e75f29ff1f4569264401c7aa007721a645105
kaz-yos <kazky@mac.com>
2014-02-05 07:31:48 -0500

    shortened code, changed helper name, added comments

--- reveal-in-finder.el
+++ reveal-in-finder.el
@@ -41,36 +41,35 @@
 In a dired buffer, it will open the current directory."
   (interactive)
   (let* ((path (buffer-file-name))
-	dir file)				; let* definition ends here
+	dir file)		   ; let* definition ends here.
     (if path
 	;; If path has been successfully obtained, set these variables.
 	(progn (setq dir (file-name-directory path))
 	       (setq file (file-name-nondirectory path)))
       ;; If path is empty, there is no file name. Use the default-directory variable.
       ;; This should work in a dired buffer.
-      (setq dir (expand-file-name default-directory)))
-    (reveal-in-finder-as dir file)
+      (setq dir (expand-file-name default-directory)))	
+    (reveal-in-finder-as dir file) ; global variables are required to pass it to the helper.
     ))
 
 ;; AppleScript helper function. Thanks milkeypostman for suggestions.
-;; Use let* to reuse revealpath in the definition of script.
+;; Use let* to reuse revealpath in defining script.
 (defun reveal-in-finder-as (dir file)
   "A helper function for reveal-in-finder.
 This function runs the actual AppleScript."
-  (let* ((revealpath (if file			; define revealpath local variable
-			 (concat dir file)	; dir/file if file name available
-		       dir))			; dir only if not
-	 (script				; define script variable using revealpath and text
+  (let* ((revealpath (if file		   ; define revealpath local variable.
+			 (concat dir file) ; dir/file if file name available.
+		       dir))		   ; dir only if not.
+	 (script			   ; define script variable using revealpath and text.
 	  (concat
 	   "set thePath to POSIX file \"" revealpath "\"\n"
 	   "tell application \"Finder\"\n"
 	   " set frontmost to true\n"
 	   " reveal thePath \n"
-	   "end tell\n")))			; let* definitions end here
-    (message script)				; check the text output
+	   "end tell\n")))		   ; let* definitions end here.
+    (message script)			   ; check the text output.
     (start-process "osascript-getinfo" nil "osascript" "-e" script)
     ))
-
 
 (provide 'reveal-in-finder)
 ;;; reveal-in-finder.el ends here
