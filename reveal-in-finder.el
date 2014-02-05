f061515e3b19e4149d95d83b121b99a1821b5369
kaz-yos <kazky@mac.com>
2014-02-05 08:28:07 -0500

    added special thanks back

--- reveal-in-finder.el
+++ reveal-in-finder.el
@@ -31,6 +31,14 @@
 ;; If M-x reveal-in-finder is invoked in a buffer not associated with a file,
 ;; it will open the folder defined in the default-directory variable.
 ;; In a dired buffer, this should open the current folder in the OS X Finder.
+;;
+;; Special thanks:
+;; This is a modified version of the open-finder found at the URL below.
+;; Thank you elemakil and lawlist for introducing this nice piece of code,
+;; http://stackoverflow.com/questions/20510333/in-emacs-how-to-show-current-file-in-finder
+;; and Peter Salazar for pointing out a useful link about AppleScript (below),
+;; http://stackoverflow.com/questions/11222501/finding-a-file-selecting-it-in-finder-issue
+;; and mikeypostman and purcell for auditing the code for MELPA approval.
 
 
 ;;; Code:
